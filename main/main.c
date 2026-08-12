b27ca15841c9fd1ba4112cbc74f7877307b18039
Sam Bonnekamp <sam@bonnekamp.net>
2026-08-13 03:21:29 +0800

    added timer-based dosing

--- main/main.c
+++ main/main.c
@@ -9,13 +9,16 @@
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "esp_err.h"
+#include <time.h>
+#include <sys/time.h>
 
+#include "esp_sntp.h"
 #include "esp_intr_alloc.h"
 #include "esp_attr.h"
 #include "driver/gptimer.h"
 #include "step_util.h"
+#include "wifi_driver.h"
 #include "prot.h"
-
 
 
 #define GPIO_OUTPUT_PIN_REG (1ULL<<PIN_STEP) | (1ULL<<PIN_DIR)          \
@@ -26,12 +29,39 @@
 #define IOBUF_SIZE 512
 
 
+uint8_t wake_driver();
+uint8_t sleep_driver();
+
 static const char *TAG = "ANDAMAN_DOSER";
 static const char *error_activelow[] = {"ERROR", "OK"};
 
 
+void set_sys_time(void){
+  struct tm tm;
+  tm.tm_year = 2026 - 1900;
+  tm.tm_mon = 7;
+  tm.tm_mday = 11;
+  tm.tm_hour = 9;
+  tm.tm_min = 33;
+  tm.tm_sec = 10;
+  //time_t t = mktime(&tm);
+  time_t t = 1786469970L;
+  //ESP_LOGI(TAG, "Setting time: %s", asctime(&tm));
+  struct timeval now = { .tv_sec = t };
+  settimeofday(&now, NULL);
+}
+
 //https://github.com/espressif/esp-idf/blob/08e0d30a/components/esp_driver_gpio/include/driver/gpio.h
 void app_main(void){
+  time_t now;
+  char strftime_buf[64];
+  struct tm timeinfo;
+  doser_schedule sched = {
+    .ml_per_dose = 2,
+    .period_s = 60,
+    .last_dose = 0
+  };  
+  
   step_struct pump_step_data = {0};
   char iobuf[IOBUF_SIZE];
   gpio_config_t output_io_conf = {
@@ -54,22 +84,49 @@ void app_main(void){
 
   ESP_LOGI(TAG, "HAIII o/");
 
+  set_sys_time();
+  time(&now);
+  setenv("TZ", "UTC+8", 1);
+  tzset();
+
+  localtime_r(&now, &timeinfo);
+  strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+  ESP_LOGI(TAG, "The current date/time is: %s", strftime_buf);
 
 
-  //wakeup driver
+  wifi_start();
+
+
+//  while(true){
+//    if((sched.last_dose + sched.period_s) < time(&now)){
+//      //wakeup driver
+//      ESP_LOGI(TAG, "driver started");       
+//      ESP_LOGI(TAG, "driver state: %s", error_activelow[wake_driver()]);
+//      gpio_set_level(PIN_LED2, 1);
+//      pump(sched.ml_per_dose, &pump_step_data);
+//      ulTaskNotifyTake(pdTRUE, portMAX_DELAY);//wait for timer isr to finish
+//      sleep_driver();
+//      gpio_set_level(PIN_LED2, 0);
+//      sched.last_dose = time(NULL);
+//    }
+//    vTaskDelay(pdMS_TO_TICKS(2000));
+//  }
+
+  
+}
+
+
+uint8_t wake_driver(){
   gpio_set_level(PIN_SLEEPB, 1);
   gpio_set_level(PIN_ENABLE, 1);
+  vTaskDelay(pdMS_TO_TICKS(50)); //driver needs a little bit of time to startup
+  return gpio_get_level(PIN_FAULTB);
+}
 
-  ESP_LOGI(TAG, "driver started");
-  ESP_LOGI(TAG, "driver state: %s", error_activelow[gpio_get_level(PIN_FAULTB)]);
-
-
-  pump(1, &pump_step_data);
-
-
-  ulTaskNotifyTake(pdTRUE, portMAX_DELAY);//wait for timer isr to finish
-
+uint8_t sleep_driver(){
+  //driver sleep
   gpio_set_level(PIN_SLEEPB, 0);
   gpio_set_level(PIN_ENABLE, 0);
-
+  vTaskDelay(pdMS_TO_TICKS(50)); //driver needs a little bit of time to startup
+  return gpio_get_level(PIN_FAULTB);
 }
--- main/prot.h
+++ main/prot.h
@@ -4,6 +4,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gptimer.h"
+#include <time.h>
 
 #define PIN_STEP 0
 #define PIN_DIR 1
@@ -14,6 +15,10 @@
 #define PIN_LED2 6
 
 
+#define SEC_PER_MIN 60
+#define SEC_PER_HR 3600
+#define AD_HOUR_TO_SECS(secs) secs * SEC_PER_HR
+
 typedef struct {
   uint16_t total_steps;
   uint16_t steps_achieved;
@@ -22,5 +27,11 @@ typedef struct {
   gptimer_handle_t gptimer;
 }step_struct;
 
+
+typedef struct {
+  float ml_per_dose;
+  uint16_t period_s;
+  time_t last_dose;
+}doser_schedule;
 
 #endif
